ccd902fd6004d0fe5cccdf8e85de4fa87dd60fea
Peder Undeli <peder.undeli@student.nhh.no>
2020-10-15 15:17:39 +0200

    Testet ut shiny

--- Shiny test.R
+++ Shiny test.R
@@ -8,18 +8,28 @@ ssb_alkohol <- ApiData("https://data.ssb.no/api/v0/no/table/04475/", ContentsCod
 alkohol_konsum <- ssb_data$dataset
 
 # Define UI ----
-ui <-fluidPage(
-  titlePanel("title panel"),
-  
-  sidebarLayout(
-    sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
-  )
+ui <- shinyUI(fluidPage(  
+  titlePanel("Alkoholkonsum per kvartal i Norge"),  
+  sidebarLayout(  
+    sidebarPanel(
+      p("Select year"),
+      sliderInput("yeartime",
+                  label="Year",
+                  min=2002,
+                  max=2020,
+                  value=2002)
+      )),
+    mainPanel(
+      plotOutput("alko_plot")  
+    )  
+  )  
 )
-
 # Define server logic ----
 server <- function(input, output) {
-  
+  output$alko_plot <- renderPlot({
+    alkohol_konsum %>% 
+      ggplot(aes(x = Tid, y = value)) +
+      geom_bar(stat = "identity")})
 }
 
 # Run the app ----
