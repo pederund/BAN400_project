90269818b6fd568b146860e21877a7f81e22c573
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-18 14:42:37 +0100

    Fikk inn XML-filen inn i en dataframe

--- Avinor.R
+++ Avinor.R
@@ -4,3 +4,5 @@ library(RCurl)
 avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=OSL"
 data_url <- getURL(avinor_url)
 data <- xmlParse(data_url)
+
+df <- xmlToDataFrame(getNodeSet(data, "//flight"))
