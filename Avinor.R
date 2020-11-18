e532094b591609e4fa5d684c16f2453539e653e5
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-18 21:20:36 +0100

    Test

--- Avinor.R
+++ Avinor.R
@@ -1,8 +1,20 @@
 library(XML)
 library(RCurl)
+library(tidyverse)
 
-avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=OSL"
+avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=OSL&TimeFrom=24&TimeTo=24"
 data_url <- getURL(avinor_url)
 data <- xmlParse(data_url)
 
-df <- xmlToDataFrame(getNodeSet(data, "//flight"))
+status_url <- "https://flydata.avinor.no/flightStatuses.asp?"
+statuscode_url <- getURL(status_url)
+status_codes <- xmlParse(statuscode_url)
+
+#df <- xmlToDataFrame(getNodeSet(data, "//flight"))
+
+df_status <- XML:::xmlAttrsToDataFrame(getNodeSet(data, c("//flight","//status")))
+#df_testing <- XML:::xmlAttrsToDataFrame(getNodeSet(data, "//status"))
+
+flight_df <- 
+  cbind(xmlToDataFrame(getNodeSet(data, "//flight")),
+        XML:::xmlAttrsToDataFrame(getNodeSet(data, "//flight")))
