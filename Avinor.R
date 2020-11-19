485f6ca3c1109f90b99da84b1b22d7d4c74c18b1
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-19 11:58:36 +0100

    Ryddet litt i datasettet, og joinet inn navn for airlines og airports

--- Avinor.R
+++ Avinor.R
@@ -28,18 +28,20 @@ flight_df <-
 status_codes_df <- 
   XML:::xmlAttrsToDataFrame(getNodeSet(status_codes, "//flightStatus"))
 
+airport_df <- 
+  XML:::xmlAttrsToDataFrame(getNodeSet(airport_names, "//airportName")) %>% 
+  rename(airport_name = name)
+
+airlines_df <- 
+  XML:::xmlAttrsToDataFrame(getNodeSet(airline_names, "//airlineName")) %>% 
+  rename(airline_name = name)
+
 full_df <- flight_df %>% 
   full_join(df_status) %>% 
   select(-status)
 
 full_df <- full_df %>% 
   full_join(status_codes_df)
-
-airport_df <- 
-  XML:::xmlAttrsToDataFrame(getNodeSet(airport_names, "//airportName"))
-
-airlines_df <- 
-  XML:::xmlAttrsToDataFrame(getNodeSet(airline_names, "//airlineName"))
 
 full_df <- full_df %>% 
   #Rydder opp i tidskolonnene
@@ -63,4 +65,13 @@ full_df <- full_df %>%
   mutate(scheduled_time = times(scheduled_time),
          updated_time = times(updated_time),
          scheduled_date = ymd(scheduled_date),
-         updated_date = ymd(updated_date))
+         updated_date = ymd(updated_date)) %>% 
+  #Renamer kolonner
+  rename(airline_code = airline,
+         airport_code = airport,
+         status_code = code,
+         status_text_EN = statusTextEn,
+         status_text_NO = statusTextNo) %>% 
+  #Joiner inn airline navn og airport navn
+  left_join(airlines_df, by = c("airline_code" = "code")) %>% 
+  left_join(airport_df, by = c("airport_code" = "code"))
