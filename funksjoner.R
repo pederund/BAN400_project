0f17863c8c7a3cf53f0ba976d06375f94021fe24
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-20 16:07:06 +0100

    Fikset sånn at lufthavner uten data blir hoppet over

--- funksjoner.R
+++ funksjoner.R
@@ -22,59 +22,65 @@ tes_func <- function(status_url, airports_url, airlines_url){
 new_function <- function(data_url, origin){
   data <- xmlParse(data_url)
   
-  df_status <- 
-    XML:::xmlAttrsToDataFrame(getNodeSet(data, c("//flight","//status"))) %>% 
-    mutate(uniqueID = lag(uniqueID)) %>% 
-    filter(!is.na(uniqueID))
+  test_df <- xmlToDataFrame(getNodeSet(data, "//flight"))
+  if(nrow(test_df) == 0){
+  }
+  else{
+    df_status <- 
+      XML:::xmlAttrsToDataFrame(getNodeSet(data, c("//flight","//status"))) %>% 
+      mutate(uniqueID = lag(uniqueID)) %>% 
+      filter(!is.na(uniqueID))
+    
+    flight_df <- 
+      bind_cols(xmlToDataFrame(getNodeSet(data, "//flight")),
+                XML:::xmlAttrsToDataFrame(getNodeSet(data, "//flight")))
+    full_df <- flight_df %>% 
+      full_join(df_status) %>% 
+      select(-status)
+    
+    full_df <- full_df %>% 
+      full_join(status_codes_df)
+    
+    full_df <- full_df %>% 
+      #Rydder opp i tidskolonnene
+      mutate(schedule_time = ymd_hms(schedule_time,
+                                     tz = ("UTC")),
+             schedule_time = force_tz(with_tz(schedule_time,
+                                              tz = "CET")),
+             time = ymd_hms(time,
+                            tz = "UTC"),
+             time = force_tz(with_tz(time,
+                                     tz = "CET"))) %>% 
+      #Skiller dato og tid fra hverandre
+      separate(schedule_time,
+               into = c("scheduled_date", "scheduled_time"),
+               sep = " ") %>% 
+      separate(time,
+               into = c("updated_date", "updated_time"),
+               sep = " ") %>% 
+      #Etter kolonnene ble separert, er de nå character. Konverterer derfor tilbake
+      #til tid og datoformat
+      mutate(scheduled_time = times(scheduled_time),
+             updated_time = times(updated_time),
+             scheduled_date = ymd(scheduled_date),
+             updated_date = ymd(updated_date)) %>% 
+      #Renamer kolonner
+      rename(airline_code = airline,
+             airport_code = airport,
+             status_code = code,
+             status_text_EN = statusTextEn,
+             status_text_NO = statusTextNo) %>% 
+      #Joiner inn airline navn og airport navn
+      left_join(airlines_df, by = c("airline_code" = "code")) %>% 
+      left_join(airport_df, by = c("airport_code" = "code")) %>% 
+      #Relocater noen kolonner for å gjøre dataframen mer leselig
+      relocate(airline_name, .after = airline_code) %>% 
+      relocate(airport_name, .after = airport_code) %>% 
+      relocate(c(status_text_NO, status_text_EN), .after = status_code) %>% 
+      mutate(origin = origin) %>% 
+      filter(!is.na(uniqueID))
+  }
   
-  flight_df <- 
-    bind_cols(xmlToDataFrame(getNodeSet(data, "//flight")),
-          XML:::xmlAttrsToDataFrame(getNodeSet(data, "//flight")))
-  full_df <- flight_df %>% 
-    full_join(df_status) %>% 
-    select(-status)
-  
-  full_df <- full_df %>% 
-    full_join(status_codes_df)
-  
-  full_df <- full_df %>% 
-    #Rydder opp i tidskolonnene
-    mutate(schedule_time = ymd_hms(schedule_time,
-                                   tz = ("UTC")),
-           schedule_time = force_tz(with_tz(schedule_time,
-                                            tz = "CET")),
-           time = ymd_hms(time,
-                          tz = "UTC"),
-           time = force_tz(with_tz(time,
-                                   tz = "CET"))) %>% 
-    #Skiller dato og tid fra hverandre
-    separate(schedule_time,
-             into = c("scheduled_date", "scheduled_time"),
-             sep = " ") %>% 
-    separate(time,
-             into = c("updated_date", "updated_time"),
-             sep = " ") %>% 
-    #Etter kolonnene ble separert, er de nå character. Konverterer derfor tilbake
-    #til tid og datoformat
-    mutate(scheduled_time = times(scheduled_time),
-           updated_time = times(updated_time),
-           scheduled_date = ymd(scheduled_date),
-           updated_date = ymd(updated_date)) %>% 
-    #Renamer kolonner
-    rename(airline_code = airline,
-           airport_code = airport,
-           status_code = code,
-           status_text_EN = statusTextEn,
-           status_text_NO = statusTextNo) %>% 
-    #Joiner inn airline navn og airport navn
-    left_join(airlines_df, by = c("airline_code" = "code")) %>% 
-    left_join(airport_df, by = c("airport_code" = "code")) %>% 
-    #Relocater noen kolonner for å gjøre dataframen mer leselig
-    relocate(airline_name, .after = airline_code) %>% 
-    relocate(airport_name, .after = airport_code) %>% 
-    relocate(c(status_text_NO, status_text_EN), .after = status_code) %>% 
-    mutate(origin = origin) %>% 
-    filter(!is.na(uniqueID))
 }
 
 final_update <- function(){
@@ -110,11 +116,11 @@ avinor_df <- data.frame(avinor_airports) %>%
   left_join(airport_df)
 
 avinor_base_url <- "https://flydata.avinor.no/XmlFeed.asp?airport="
-avinor_urls <- paste0(avinor_base_url, our_airports)
+avinor_urls <- paste0(avinor_base_url, avinor_airports)
 origin <- stringr::str_sub(avinor_urls, -3, -1)
 data_url <- map(avinor_urls, getURL)
 
-for (i in 1:length(our_airports)){
+for (i in 1:length(avinor_airports)){
   full_df <- new_function(data_url[[i]], origin[i])
   final_update()
 }
