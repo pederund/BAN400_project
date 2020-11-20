704700e9032bea08af66a3ffa370fe3cd883e337
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-20 10:45:38 +0100

    Laget en funksjon som binder sammen df for hver flyplass

--- Avinor.R
+++ Avinor.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(lubridate)
 library(chron)
 
-avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=OSL&TimeFrom=24&TimeTo=24"
+avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=OSL"
 data_url <- getURL(avinor_url)
 data <- xmlParse(data_url)
 
@@ -75,9 +75,11 @@ full_df <- full_df %>%
   #Joiner inn airline navn og airport navn
   left_join(airlines_df, by = c("airline_code" = "code")) %>% 
   left_join(airport_df, by = c("airport_code" = "code")) %>% 
+  #Relocater noen kolonner for å gjøre dataframen mer leselig
   relocate(airline_name, .after = airline_code) %>% 
   relocate(airport_name, .after = airport_code) %>% 
-  relocate(c(status_text_NO, status_text_EN), .after = status_code)
+  relocate(c(status_text_NO, status_text_EN), .after = status_code) %>% 
+  mutate(origin = stringr::str_sub(avinor_url, -3, -1))
 
 avinor_airports <- c("OSL", "BGO", "KRS", "VDB", "KSU", "MOL", "HOV", "AES", "ANX",
                      "BOO", "BNN", "EVE", "LKN", "MQN", "MJF", "RET", "SSJ", "SKN",
@@ -87,3 +89,21 @@ avinor_airports <- c("OSL", "BGO", "KRS", "VDB", "KSU", "MOL", "HOV", "AES", "AN
 avinor_df <- data.frame(avinor_airports) %>% 
   rename(code = avinor_airports) %>% 
   left_join(airport_df)
+
+#Lager en tom dataframe "final_df", hvis den ikke allerede eksisterer.
+#Dette for å forhindre at "final_df" blir slettet og overskrevet.
+if (!exists("final_df")){
+  final_df <- data.frame()
+}
+  
+#En funksjon som sjekker om det er noe i final_df, hvis ikke så legger den inn full_df,
+#dvs. første gang legges full_df inn.
+#Hvis det er noe i final_df, kjøres bind_rows på det som er full_df den iterasjonen.
+final_update <- function(){
+    if(nrow(final_df) == 0)
+      final_df <<- full_df
+    else
+      final_df <<- bind_rows(final_df, full_df) 
+}
+
+final_update()
