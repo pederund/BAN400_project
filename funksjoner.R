79bdc32f249509add63eb81489d90b81d241f82e
Peder Undeli <peder.undeli@student.nhh.no>
2020-11-20 11:50:15 +0100

    Bakt alt inn i funksjoner

---
+++ funksjoner.R
@@ -0,0 +1,103 @@
+tes_func <- function(status_url, airports_url, airlines_url){
+  status_codes <- xmlParse(status_url)
+  airport_names <- xmlParse(airports_url)
+  airline_names <- xmlParse(airlines_url)
+  
+  list_dfs <- list()
+  
+  list_dfs$status_codes_df <- 
+    XML:::xmlAttrsToDataFrame(getNodeSet(status_codes, "//flightStatus"))
+  
+  list_dfs$airport_df <- 
+    XML:::xmlAttrsToDataFrame(getNodeSet(airport_names, "//airportName")) %>% 
+    rename(airport_name = name)
+  
+  list_dfs$airlines_df <- 
+    XML:::xmlAttrsToDataFrame(getNodeSet(airline_names, "//airlineName")) %>% 
+    rename(airline_name = name)
+  
+  return(list_dfs)
+}
+
+new_function <- function(data_url){
+  data <- xmlParse(data_url)
+  
+  df_status <- 
+    XML:::xmlAttrsToDataFrame(getNodeSet(data, c("//flight","//status"))) %>% 
+    mutate(uniqueID = lag(uniqueID)) %>% 
+    filter(!is.na(uniqueID))
+  
+  flight_df <- 
+    cbind(xmlToDataFrame(getNodeSet(data, "//flight")),
+          XML:::xmlAttrsToDataFrame(getNodeSet(data, "//flight")))
+  full_df <- flight_df %>% 
+    full_join(df_status) %>% 
+    select(-status)
+  
+  full_df <- full_df %>% 
+    full_join(status_codes_df)
+  
+  full_df <- full_df %>% 
+    #Rydder opp i tidskolonnene
+    mutate(schedule_time = ymd_hms(schedule_time,
+                                   tz = ("UTC")),
+           schedule_time = force_tz(with_tz(schedule_time,
+                                            tz = "CET")),
+           time = ymd_hms(time,
+                          tz = "UTC"),
+           time = force_tz(with_tz(time,
+                                   tz = "CET"))) %>% 
+    #Skiller dato og tid fra hverandre
+    separate(schedule_time,
+             into = c("scheduled_date", "scheduled_time"),
+             sep = " ") %>% 
+    separate(time,
+             into = c("updated_date", "updated_time"),
+             sep = " ") %>% 
+    #Etter kolonnene ble separert, er de nå character. Konverterer derfor tilbake
+    #til tid og datoformat
+    mutate(scheduled_time = times(scheduled_time),
+           updated_time = times(updated_time),
+           scheduled_date = ymd(scheduled_date),
+           updated_date = ymd(updated_date)) %>% 
+    #Renamer kolonner
+    rename(airline_code = airline,
+           airport_code = airport,
+           status_code = code,
+           status_text_EN = statusTextEn,
+           status_text_NO = statusTextNo) %>% 
+    #Joiner inn airline navn og airport navn
+    left_join(airlines_df, by = c("airline_code" = "code")) %>% 
+    left_join(airport_df, by = c("airport_code" = "code")) %>% 
+    #Relocater noen kolonner for å gjøre dataframen mer leselig
+    relocate(airline_name, .after = airline_code) %>% 
+    relocate(airport_name, .after = airport_code) %>% 
+    relocate(c(status_text_NO, status_text_EN), .after = status_code)
+}
+
+final_update <- function(){
+  if(nrow(final_df) == 0)
+    final_df <<- full_df
+  else
+    final_df <<- bind_rows(final_df, full_df) 
+}
+
+if (!exists("final_df")){
+  final_df <- data.frame()
+}
+
+status_url <- getURL("https://flydata.avinor.no/flightStatuses.asp?")
+airports_url <- getURL("https://flydata.avinor.no/airportNames.asp?")
+airlines_url <- getURL("https://flydata.avinor.no/airlineNames.asp")
+
+test_dfs <- tes_func(status_url, airports_url, airlines_url)
+status_codes_df <- test_dfs$status_codes_df
+airport_df <- test_dfs$airport_df
+airlines_df <- test_dfs$airlines_df
+
+avinor_url <- "https://flydata.avinor.no/XmlFeed.asp?airport=FRO"
+data_url <- getURL(avinor_url)
+
+full_df <- new_function(data_url)
+
+final_update()
