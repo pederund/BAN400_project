c92ca0220d5d611dbe734cd2b4c24ec81a58682d
Peder Undeli <peder.undeli@student.nhh.no>
2020-12-10 13:54:54 +0100

    Lagt til tabs, tatt vekk radiobuttons

--- avinor_shiny Peder.r
+++ avinor_shiny Peder.r
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinyjs)
+library(shinyalert)
 library(stringr)
 library(DT)
 
@@ -8,22 +9,20 @@ first_run <- 1
 # defining user interface and input variables
 ui <- fluidPage(
   shinyjs::useShinyjs(),
-  titlePanel('Flight info position 1'),
+  shinyalert::useShinyalert(),
+  titlePanel(h1('Avinor flight data', align = "center")),
+  br(),
   
   sidebarLayout(
     sidebarPanel(
-      helpText('Flight info position 2'),
+      helpText('Data will be automatically updated every three minutes.'),
       
       selectInput(inputId = 'airport',
                   label = 'Select airport',
                   choices = avinor_df %>% select(airport_name) %>% arrange(airport_name),
                   selected = avinor_df$airport_name[1]),
-    
-      radioButtons(inputId = 'arrdep',
-                   label = "Arrival / Departure",
-                   choices = c("Arrival", "Departure"),
-                   selected = "Arrival"),
-    
+      
+      
       checkboxGroupInput(inputId = "domint",
                          label = "Domestic / International",
                          choices = c("Domestic", "International"),
@@ -41,15 +40,34 @@ ui <- fluidPage(
     mainPanel(actionButton("viewButton", "Show earlier flights",
                            style = "color: white; background-color: #84216b;
                                     position:relative;left:350px"),
+              
               actionButton("resetviewButton", "Hide earlier flights",
                            style = "color: white; background-color: #84216b;
                                     position:relative;left:350px"),
-              tags$style(HTML("
-                              thead {background-color:#f0f1f3;}
-                              table.dataTable tbody tr {background-color:white !important}"
-                              )),
-              dataTableOutput(outputId = 'df'),
-              width = 9)))
+              br(),
+              br(),
+              
+              tabsetPanel(
+                id = 'dataset',
+                tabPanel("Arrival",dataTableOutput("table1")),
+                tabPanel("Departure", dataTableOutput("table2"))),
+              
+              br(),
+              
+              dataTableOutput(outputId = 'df', width = "100%"),
+              width = 9)),
+  
+  tags$style(HTML("
+  thead {background-color:#f0f1f3;}
+  tbody tr {background-color:white !important}
+  .dataTables_wrapper {background-color: #f0f1f3;}
+  .tabbable > .nav > li[class=active]  > a {background-color:#f0f1f3;   color:black}
+  .tabbable > .nav > li > a {color:black; width: 35vw !important;} 
+  "
+  ))
+  
+)
+
 
 # defining what to output
 server <- function(input, output) {
@@ -62,191 +80,196 @@ server <- function(input, output) {
   options(DT.options = list(pageLength = 30, 
                             dom = "ftp",
                             ordering = FALSE,
+                            searchHighlight = TRUE,
                             language = list(zeroRecords = "No flights to display",
                                             search = "Search (city, flight number or airline):")))
   
   #Creating a reactive object, which will be updated every 3 minutes with data from
   #Avinor through running the function to get the data at a set interval
   final_dfReactive <-  reactive({
-    invalidateLater(30000)
+    invalidateLater(180000)
     if (first_run == 1){
       first_run <<- first_run + 1
       return(final_df)
     } else{
-      run_function() 
+      shinyalert(
+        title = "Updating data", text = "This may take a few seconds", type = "info",
+        closeOnClickOutside = TRUE, animation = TRUE, timer = 5000,
+        showConfirmButton = FALSE
+      )
+      run_function()
     }
   })
   
   #default table output
-  output$df <- renderDataTable({
-    if(input$arrdep == "Arrival"){
-      final_dfReactive() %>%
-        filter(origin == as.character(
-          avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-        filter(arr_dep == "A") %>% 
-        filter(dom_int %in%
-                 if(length(input$domint) == 2 | length(input$domint) == 0) {
-                   c("D", "S", "I")
-                 } else if(input$domint == "International") {
-                   c("S", "I")
-                 } else {
-                   "D"
-                 }) %>%
-        filter(scheduled_date == input$date) %>%
-        filter(
-          if(input$date == Sys.Date()){
-            scheduled_time >= chron::times(str_sub(Sys.time(), 12))
-          }
-          else{
-            scheduled_time >= chron::times(00:00:00)
-          }) %>%
-        select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
-        rename("Updated status" = updated_flightstatus,
-               "From airport" = airport_name,
-               "Belt" = belt_html,
-               "Flight" = flight_html)
-    }
-    else{
-      final_dfReactive() %>%
-        filter(origin == as.character(
-          avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-        filter(arr_dep == "D") %>% 
-        filter(dom_int %in%
-                 if(length(input$domint) == 2 | length(input$domint) == 0) {
-                   c("D", "S", "I")
-                 } else if(input$domint == "International") {
-                   c("S", "I")
-                 } else {
-                   "D"
-                 }) %>%
-        filter(scheduled_date == input$date) %>%
-        filter(
-          if(input$date == Sys.Date()){
-            scheduled_time >= chron::times(stringr::str_sub(Sys.time(), 12))
-          }
-          else{
-            scheduled_time >= chron::times(00:00:00)
-          }) %>%
-        select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
-        rename("Updated status" = updated_flightstatus,
-               "To airport" = airport_name,
-               "Gate" = gate_html,
-               "Flight" = flight_html)
-    }
-    
-  }, escape = FALSE, rownames = FALSE) 
-  
-#### If the show earlier flights button is clicked ####
-  observeEvent(
-    input$viewButton,{
-      output$df <- renderDataTable({
-        if(input$arrdep == "Arrival"){
-          final_dfReactive() %>%
-            filter(origin == as.character(
-              avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-            filter(arr_dep == "A") %>% 
-            filter(dom_int %in%
-                     if(length(input$domint) == 2 | length(input$domint) == 0) {
-                       c("D", "S", "I")
-                     } else if(input$domint == "International") {
-                       c("S", "I")
-                     } else {
-                       "D"
-                     }) %>%
-            filter(scheduled_date == input$date) %>%
-            select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
-            rename("Updated status" = updated_flightstatus,
-                   "From airport" = airport_name,
-                   "Belt" = belt_html,
-                   "Flight" = flight_html)
+  output$table1 <- renderDataTable({
+    final_dfReactive() %>%
+      filter(origin == as.character(
+        avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+      filter(arr_dep == "A") %>% 
+      filter(dom_int %in%
+               if(length(input$domint) == 2 | length(input$domint) == 0) {
+                 c("D", "S", "I")
+               } else if(input$domint == "International") {
+                 c("S", "I")
+               } else {
+                 "D"
+               }) %>%
+      filter(scheduled_date == input$date) %>%
+      filter(
+        if(input$date == Sys.Date()){
+          scheduled_time >= chron::times(str_sub(Sys.time()-1*60*60, 12))
         }
         else{
-          final_dfReactive() %>%
-            filter(origin == as.character(
-              avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-            filter(arr_dep == "D") %>% 
-            filter(dom_int %in%
-                     if(length(input$domint) == 2 | length(input$domint) == 0) {
-                       c("D", "S", "I")
-                     } else if(input$domint == "International") {
-                       c("S", "I")
-                     } else {
-                       "D"
-                     }) %>%
-            filter(scheduled_date == input$date) %>%
-            select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
-            rename("Updated status" = updated_flightstatus,
-                   "To airport" = airport_name,
-                   "Gate" = gate_html,
-                   "Flight" = flight_html)
+          scheduled_time >= chron::times(00:00:00)
+        }) %>%
+      select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
+      rename("Updated status" = updated_flightstatus,
+             "From airport" = airport_name,
+             "Belt" = belt_html,
+             "Flight" = flight_html)
+  }, escape = FALSE, rownames = FALSE)
+  
+  output$table2 <- renderDataTable({
+    final_dfReactive() %>%
+      filter(origin == as.character(
+        avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+      filter(arr_dep == "D") %>% 
+      filter(dom_int %in%
+               if(length(input$domint) == 2 | length(input$domint) == 0) {
+                 c("D", "S", "I")
+               } else if(input$domint == "International") {
+                 c("S", "I")
+               } else {
+                 "D"
+               }) %>%
+      filter(scheduled_date == input$date) %>%
+      filter(
+        if(input$date == Sys.Date()){
+          scheduled_time >= chron::times(stringr::str_sub(Sys.time()-1*60*60, 12))
         }
+        else{
+          scheduled_time >= chron::times(00:00:00)
+        }) %>%
+      select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
+      rename("Updated status" = updated_flightstatus,
+             "To airport" = airport_name,
+             "Gate" = gate_html,
+             "Flight" = flight_html)
+    
+  }, escape = FALSE, rownames = FALSE)
+  
+  #### If the show earlier flights button is clicked ####
+  observeEvent(
+    input$viewButton,{
+      output$table1 <- renderDataTable({
+        final_dfReactive() %>%
+          filter(origin == as.character(
+            avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+          filter(arr_dep == "A") %>% 
+          filter(dom_int %in%
+                   if(length(input$domint) == 2 | length(input$domint) == 0) {
+                     c("D", "S", "I")
+                   } else if(input$domint == "International") {
+                     c("S", "I")
+                   } else {
+                     "D"
+                   }) %>%
+          filter(scheduled_date == input$date) %>%
+          select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
+          rename("Updated status" = updated_flightstatus,
+                 "From airport" = airport_name,
+                 "Belt" = belt_html,
+                 "Flight" = flight_html)
+      }, escape = FALSE, rownames = FALSE)
+      
+      output$table2 <- renderDataTable({
+        final_dfReactive() %>%
+          filter(origin == as.character(
+            avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+          filter(arr_dep == "D") %>% 
+          filter(dom_int %in%
+                   if(length(input$domint) == 2 | length(input$domint) == 0) {
+                     c("D", "S", "I")
+                   } else if(input$domint == "International") {
+                     c("S", "I")
+                   } else {
+                     "D"
+                   }) %>%
+          filter(scheduled_date == input$date) %>%
+          select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
+          rename("Updated status" = updated_flightstatus,
+                 "To airport" = airport_name,
+                 "Gate" = gate_html,
+                 "Flight" = flight_html)
         
       }, escape = FALSE, rownames = FALSE)
       hide("viewButton")
       show("resetviewButton")
     })
+  
   observeEvent(
     input$resetviewButton,{
-      output$df <- renderDataTable({
-        if(input$arrdep == "Arrival"){
-          final_dfReactive() %>%
-            filter(origin == as.character(
-              avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-            filter(arr_dep == "A") %>% 
-            filter(dom_int %in%
-                     if(length(input$domint) == 2 | length(input$domint) == 0) {
-                       c("D", "S", "I")
-                     } else if(input$domint == "International") {
-                       c("S", "I")
-                     } else {
-                       "D"
-                     }) %>%
-            filter(scheduled_date == input$date) %>%
-            filter(
-              if(input$date == Sys.Date()){
-                scheduled_time >= chron::times(str_sub(Sys.time(), 12))
-              }
-              else{
-                scheduled_time >= chron::times(00:00:00)
-              }) %>%
-            select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
-            rename("Updated status" = updated_flightstatus,
-                   "From airport" = airport_name,
-                   "Belt" = belt_html,
-                   "Flight" = flight_html)
-        }
-        else{
-          final_dfReactive() %>%
-            filter(origin == as.character(
-              avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
-            filter(arr_dep == "D") %>% 
-            filter(dom_int %in%
-                     if(length(input$domint) == 2 | length(input$domint) == 0) {
-                       c("D", "S", "I")
-                     } else if(input$domint == "International") {
-                       c("S", "I")
-                     } else {
-                       "D"
-                     }) %>%
-            filter(scheduled_date == input$date) %>% 
-            filter(
-              if(input$date == Sys.Date()){
-                scheduled_time >= chron::times(stringr::str_sub(Sys.time(), 12))
-              }
-              else{
-                scheduled_time >= chron::times(00:00:00)
-              }) %>%
-            select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
-            rename("Updated status" = updated_flightstatus,
-                   "To airport" = airport_name,
-                   "Gate" = gate_html,
-                   "Flight" = flight_html)
-        }
+      output$table1 <- renderDataTable({
+        final_dfReactive() %>%
+          filter(origin == as.character(
+            avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+          filter(arr_dep == "A") %>% 
+          filter(dom_int %in%
+                   if(length(input$domint) == 2 | length(input$domint) == 0) {
+                     c("D", "S", "I")
+                   } else if(input$domint == "International") {
+                     c("S", "I")
+                   } else {
+                     "D"
+                   }) %>%
+          filter(scheduled_date == input$date) %>%
+          filter(
+            if(input$date == Sys.Date()){
+              scheduled_time >= chron::times(str_sub(Sys.time()-1*60*60, 12))
+            }
+            else{
+              scheduled_time >= chron::times(00:00:00)
+            }) %>%
+          select(updated_flightstatus ,airport_name, flight_html ,belt_html) %>% 
+          rename("Updated status" = updated_flightstatus,
+                 "From airport" = airport_name,
+                 "Belt" = belt_html,
+                 "Flight" = flight_html)
+      }, escape = FALSE, rownames = FALSE)
+      
+      output$table2 <- renderDataTable({
+        final_dfReactive() %>%
+          filter(origin == as.character(
+            avinor_df[(avinor_df$airport_name == input$airport), ][1])) %>% 
+          filter(arr_dep == "D") %>% 
+          filter(dom_int %in%
+                   if(length(input$domint) == 2 | length(input$domint) == 0) {
+                     c("D", "S", "I")
+                   } else if(input$domint == "International") {
+                     c("S", "I")
+                   } else {
+                     "D"
+                   }) %>%
+          filter(scheduled_date == input$date) %>%
+          filter(
+            if(input$date == Sys.Date()){
+              scheduled_time >= chron::times(stringr::str_sub(Sys.time()-1*60*60, 12))
+            }
+            else{
+              scheduled_time >= chron::times(00:00:00)
+            }) %>%
+          select(updated_flightstatus, airport_name ,flight_html ,gate_html) %>%
+          rename("Updated status" = updated_flightstatus,
+                 "To airport" = airport_name,
+                 "Gate" = gate_html,
+                 "Flight" = flight_html)
         
       }, escape = FALSE, rownames = FALSE)
       hide("resetviewButton")
       show("viewButton")
-    })
+    })  
+  
 }
 
 # run app
