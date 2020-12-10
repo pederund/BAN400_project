164d62cc665d3e61ca872d2dd193ecf6b7506a4d
Peder Undeli <peder.undeli@student.nhh.no>
2020-12-10 13:53:30 +0100

    Lagt til docstring + litt opprydding i koden

--- flight_data.R
+++ flight_data.R
@@ -5,10 +5,10 @@ library(tidyverse)
 library(lubridate)
 library(chron)
 library(stringr)
-library(tcltk2)
+library(docstring)
 
 tes_func <- function(status_url, airports_url, airlines_url){
-  status_codes <- xmlParse(status_url)
+  status_codes <- xmlParse(status_url, encoding = "ISO-8859-1")
   airport_names <- read_xml(airports_url, encoding = "ISO-8859-1")
   airline_names <- read_xml(airlines_url, encoding = "ISO-8859-1")
   
@@ -24,27 +24,35 @@ tes_func <- function(status_url, airports_url, airlines_url){
       code = xml_attr(airport_kids, "code"),
       airport_name = xml_attr(airport_kids, "name")
     )
-  #XML:::xmlAttrsToDataFrame(getNodeSet(airport_names, "//airportName")) %>% 
-  #rename(airport_name = name)
   
   list_dfs$airlines_df <- 
     data.frame(
       code = xml_attr(airline_kids, "code"),
       airline_name = xml_attr(airline_kids, "name")
     )
-  #XML:::xmlAttrsToDataFrame(getNodeSet(airline_names, "//airlineName")) %>% 
-  #rename(airline_name = name)
   
   return(list_dfs)
 }
 
-new_function <- function(data_url, origin){
-  data <- xmlParse(data_url)
+get_flightdata <- function(xml_data, origin){
+  #' Get Avinor flight data
+  #'
+  #' Retrieves flight data from the Avinor "flydata" service, and turns the xml-data
+  #' into a dataframe object.
+  #' 
+  #' @details Remember to run the URL to the xml-data through getURL first
+  #' 
+  #' @param xml_data The xml-data for the flight data
+  #' @param origin IATA code for the airport the flight data is from
+  #' 
+  #' @usage get_flightdata(xml_data, origin)
+  #' 
+  #' @examples 
+  #' osl_xml <- getURL("https://flydata.avinor.no/XmlFeed.asp?airport=OSL&TimeFrom=24&TimeTo=24")
+  #' get_flightdata(osl_xml, "OSL")
   
-  #test_df <- xmlToDataFrame(getNodeSet(data, "//flight"))
-  #if(nrow(test_df) == 0){
-  #}
-  #else{
+  data <- xmlParse(xml_data)
+  
   df_status <- 
     XML:::xmlAttrsToDataFrame(getNodeSet(data, c("//flight","//status"))) %>% 
     mutate(uniqueID = lag(uniqueID)) %>% 
@@ -58,11 +66,10 @@ new_function <- function(data_url, origin){
     full_join(df_status) %>% 
     mutate(origin = origin) %>% 
     filter(!is.na(uniqueID))
-  
-  #}
+
 }
 
-
+# Vector containing all the IATA-codes of Avinor's airports
 avinor_airports <- c("OSL", "BGO", "KRS", "BDU", "KSU", "MOL", "HOV", "AES", "ANX",
                      "BOO", "BNN", "EVE", "LKN", "MQN", "MJF", "RET", "SSJ", "SKN",
                      "SVJ", "VRY", "HAU", "SVG", "LYR", "OSY", "RRS", "RVK", "TRD",
@@ -84,18 +91,19 @@ status_codes_df <- test_dfs$status_codes_df
 airport_df <- test_dfs$airport_df
 airlines_df <- test_dfs$airlines_df
 
+# Creating a dataframe consisting of only Avinor's airports
 avinor_df <- data.frame(avinor_airports) %>% 
   rename(code = avinor_airports) %>% 
   left_join(airport_df) %>% 
   mutate(airport_name = str_replace(airport_name, pattern = "Ã¸", replacement = "ø"),
          airport_name = str_replace(airport_name, pattern = "Ã¦", replacement = "æ"))
 
+
 avinor_base_url <- "https://flydata.avinor.no/XmlFeed.asp?airport="
 avinor_urls <- paste0(avinor_base_url, avinor_airports, "&TimeFrom=24&TimeTo=24")
 
 run_function <- function(){
-  
-  #data_url <- map(.x = avinor_urls, ~getURL(., .encoding = "ISO-8859-1"))
+
   # linja under henter inn url'ene kjappere, pga async = TRUE, da laster 
   # getURL funksjonen inn de 44 urlene samtidig
   data_url <- getURL(avinor_urls, async = TRUE, .encoding = "ISO-8859-1")
@@ -104,7 +112,7 @@ run_function <- function(){
     # creating the dataframe
     # map2dfr lar oss loope over 2 inputs samtidig. Her looper vi altså over både
     # data_url og avinor_airports.
-    map2_dfr(.x = ., .y = avinor_airports ,~new_function(.x, .y)) %>% 
+    map2_dfr(.x = ., .y = avinor_airports ,~get_flightdata(.x, .y)) %>% 
     
     ## joining in dataframes containing airline- & airport names and also text for 
     ## each status code
@@ -155,15 +163,7 @@ run_function <- function(){
     unite(flight_html, c(flight_id_html, airline_name),
           sep = "<br>", remove = FALSE, na.rm = TRUE)
   
-  # assigning final_df to the global environment
-  .GlobalEnv$final_df <- final_df
 }
 
 
-run_function()
-
-#Linja under kjÃ¸rer funksjonen for Ã¥ oppdatere hvert tredje minutt, helt til vi
-#sier at den skal stoppe.
-#Det gjÃ¸res ved: tclTaskDelete("testing")
-
-#tclTaskSchedule(180000, run_function(), id = "testing", redo = TRUE)
+final_df <- run_function()
